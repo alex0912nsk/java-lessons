0e0b7549e35c9fcbd5062dc33caff97c6f149959
FTC\avasilev <a.vasilev@cft.ru>
2025-03-14 00:30:50 +0700

    домашка 1

---
+++ src/main/java/korona/bk/homework/task1/Sort.java
@@ -0,0 +1,56 @@
+package korona.bk.homework.task1;
+
+public class Sort {
+
+    private Sort() {
+    }
+
+    public static void sortBubbleAscent(int[] array) {
+        boolean hasChange = true;
+        while (hasChange) {
+            hasChange = false;
+            for (int i = 1; i < array.length; i++) {
+                if (array[i - 1] > array[i]) {
+                    swap(array, i - 1, i);
+                    hasChange = true;
+                }
+            }
+        }
+    }
+
+    public static void sortBubbleAscent(char[] array) {
+        boolean hasChange = true;
+        while (hasChange) {
+            hasChange = false;
+            for (int i = 1; i < array.length; i++) {
+                if (array[i - 1] > array[i]) {
+                    swap(array, i - 1, i);
+                    hasChange = true;
+                }
+            }
+        }
+    }
+
+    public static void sortBubbleAscent(char[][] array) {
+        char[] sortArray = new char[array.length * array[0].length];
+        for (int i = 0; i < array.length; i++) {
+            System.arraycopy(array[i], 0, sortArray, i * array[i].length, array[i].length);
+        }
+        sortBubbleAscent(sortArray);
+        for (int i = 0; i < array.length; i++) {
+            System.arraycopy(sortArray, i * array[i].length, array[i], 0, array[i].length);
+        }
+    }
+
+    private static void swap(int[] a, int i, int j) {
+        int t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+    }
+
+    private static void swap(char[] a, int i, int j) {
+        char t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+    }
+}
